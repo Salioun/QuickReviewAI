f655e876e21adffecc5a2399167a62428d95cb65
Salif N'diaye <salioun01@gmail.com>
2026-04-05 03:53:56 -0400

    ajout celeris et redis

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -132,3 +132,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Celery
+CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://redis:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://redis:6379/0')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+
+# GitHub
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN', '')
